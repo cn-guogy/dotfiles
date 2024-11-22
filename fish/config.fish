ff023be444a09d7f208c7525acb1a644be980332
guogy <cn.ggy@qq.com>
2024-11-22 10:06:29 +0800

    chage config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 alias ff="fastfetch -c examples/11"
+alias setproxy="export http_proxy=http://127.0.0.1:7890/; export https_proxy=http://127.0.0.1:7890/; echo 'Set proxy successfully'"
+alias unsetproxy="unset http_proxy; unset https_proxy; echo 'Unset proxy successfully'"
 
 if status is-interactive
-    export GPG_TTF=$(tty)
     # Commands to run in interactive sessions can go here
 end
